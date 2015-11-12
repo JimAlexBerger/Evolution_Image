bfde4fb194d9487923c4cb5a97a2a5d2a6c43808
Jim-alexander Berger Gundersen <alexandergundersen@yahoo.no>
2015-11-12 15:46:46 +0100

    added a fitness function called likeness, lower is better

--- Evolution_Image/Form1.cs
+++ Evolution_Image/Form1.cs
@@ -32,11 +32,17 @@ namespace Evolution_Image
 
             Source_Imagebox.BackgroundImage = source_Image;
 
-            Bitmap evo_Image = new Bitmap(source_Image.Width,source_Image.Height);
+            Bitmap evo_Image = new Bitmap(source_Image.Width, source_Image.Height);
 
             for (int i = 0; i < 10; i++)
             {
                 Triangle triangle = new Triangle(evo_Image);
+
+                triangle.pos[1] = new Point(12, 12);
+                triangle.r = 0;
+                triangle.g = 170;
+                triangle.b = 0;
+                triangle.a = 0;
 
                 using (Graphics g = Graphics.FromImage(evo_Image))
                 {
@@ -44,19 +50,108 @@ namespace Evolution_Image
                     g.FillPolygon(drawing_Evo_Brush, triangle.pos);
                     //g.FillRectangle(drawing_Evo_Brush, 5, i, 5, 1);
                 }
-                Thread.Sleep(10);
+                //Thread.Sleep(10);
                 Evo_Imagebox.BackgroundImage = evo_Image;
                 
-            }                 
+            }
+            for (int v = 0; v < 1; v++)
+            {
+                Genes_Textbox.Text += ("Likeness: " + likeness(source_Image, evo_Image) + Environment.NewLine);
+            }
+        }
+
+        public int likeness(Bitmap source, Bitmap compare)
+        {
+            if ((source.Height == compare.Height) && (source.Width == compare.Width))
+            {
+                int fitness = 0;
+                Color clr_Source = new Color();
+                Color clr_Compare = new Color();
+                for (int x = 0; x < source.Width; x++)
+                {
+                    for (int y = 0; y < source.Height; y++)
+                    {
+                        clr_Source = source.GetPixel(x, y);
+                        clr_Compare = compare.GetPixel(x, y);
+                        fitness += Math.Abs(clr_Source.R - clr_Compare.R);
+                        fitness += Math.Abs(clr_Source.G - clr_Compare.G);
+                        fitness += Math.Abs(clr_Source.B - clr_Compare.B);
+                    }
+                }
+                return fitness;
+            }
+            else return 0;
+        }
+
+        public double likeness_percent(Bitmap source, Bitmap compare)
+        {
+            int like = likeness(source, compare);
+
+            int worst = 0;
+
+            for (int x = 0; x < source.Width; x++)
+            {
+                for (int y = 0; y < source.Height; y++)
+                {
+                    worst += 3 * 255;
+
+                }
+
+            }
+
+            double fitness_Percent = 100 - ((like + 0.0) / (worst + 0.0));
+            return fitness_Percent;
+
         }
 
         public class Triangle
         {
             public Point[] pos { get; set; }
-            public int r { get; set; }
-            public int g { get; set; }
-            public int b { get; set; }
-            public int a { get; set; }
+
+            public int r;
+            public int R
+            {
+                get { return r; }
+                set
+                {
+                    this.r = R;
+                    this.color = Color.FromArgb(this.a, this.r, this.g, this.b);
+                }
+            }
+
+            public int g;
+            public int G
+            {
+                get { return g; }
+                set
+                {
+                    this.g = G;
+                    this.color = Color.FromArgb(this.a, this.r, this.g, this.b);
+                }
+            }
+
+            public int b;
+            public int B
+            {
+                get { return b; }
+                set
+                {
+                    this.b = B;
+                    this.color = Color.FromArgb(this.a, this.r, this.g, this.b);
+                }
+            }
+
+            public int a;
+            public int A
+            {
+                get { return a; }
+                set
+                {
+                    this.a = A;
+                    this.color = Color.FromArgb(this.a, this.r, this.g, this.b);
+                }
+            }
+
             public Color color { get; set; }
 
             public Triangle(Bitmap Source)
@@ -74,5 +169,6 @@ namespace Evolution_Image
             }
 
         }
+
     }
 }
